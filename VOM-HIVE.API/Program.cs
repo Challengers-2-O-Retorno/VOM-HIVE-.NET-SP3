d69a7bd5a4a64b0f5b99a28f2e08c893e397736e
gabrielrodri33 <gabrielrodri333@gmail.com>
2024-09-12 21:26:21 -0300

    Configurado conexão com o banco de dados

--- VOM-HIVE.API/Program.cs
+++ VOM-HIVE.API/Program.cs
@@ -1,9 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using VOM_HIVE.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseOracle(builder.Configuration.GetConnectionString("OracleConnection"));
+});
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
