75f4f7608983893eddbdd882c7f502634cbe6ede
gabrielrodri33 <gabrielrodri333@gmail.com>
2024-09-14 15:30:57 -0300

    Criado DTO para o Edit da tabela Product

---
+++ VOM-HIVE.API/DTO/Product/ProductEditDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace VOM_HIVE.API.DTO.Product
+{
+    public class ProductEditDto
+    {
+        public int id_product { get; set; }
+        public string? nm_product { get; set; }
+        public string? category_product { get; set; }
+    }
+}
