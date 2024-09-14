18c4525612a7783d54b911e0d443b438fbbfb58e
gabrielrodri33 <gabrielrodri333@gmail.com>
2024-09-14 15:31:38 -0300

    Criado a Interface de Product para realizar as operações de CRUD

---
+++ VOM-HIVE.API/Services/Product/IProductInterface.cs
@@ -0,0 +1,16 @@
+﻿using Sp2.Models;
+using VOM_HIVE.API.DTO.Product;
+using VOM_HIVE.API.Models;
+
+namespace VOM_HIVE.API.Services.Product
+{
+    public interface IProductInterface
+    {
+        Task<ResponseModel<List<ProductModel>>> ListarProdutos();
+        Task<ResponseModel<ProductModel>> BuscaProdutoPorId(int id_product);
+        Task<ResponseModel<ProductModel>> BuscarProdutoPorIdCampaign(int id_campaign);
+        Task<ResponseModel<List<ProductModel>>> CreateProduct(ProductCreateDto productCreateDto);
+        Task<ResponseModel<List<ProductModel>>> EditProduct(ProductEditDto productEditDto);
+        Task<ResponseModel<List<ProductModel>>> DeleteProduct(int id_product);
+    }
+}
