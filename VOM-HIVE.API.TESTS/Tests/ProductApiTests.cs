bbba44b497768931364abcdbdbcd122323b205be
jixatos <j1108gody@outlook.com>
2024-10-25 01:25:35 -0300

    Criação dos testes dos endpoints de GET

--- VOM-HIVE.API.TESTS/Tests/ProductApiTests.cs
+++ VOM-HIVE.API.TESTS/Tests/ProductApiTests.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages.Manage;
 using Sp2.Models;
+using System.Net;
 using System.Net.Http.Json;
 using VOM_HIVE.API.Data;
 using VOM_HIVE.API.DTO.Product;
@@ -27,8 +29,17 @@ namespace VOM_HIVE.API.TESTS.Tests
         public async Task GetProducts_ReturnsListOfProducts()
         {
             // Arrange
-            _context.Product.Add(new ProductModel { nm_product = "produtoTeste1", category_product = "categoriaProduto1" });
-            _context.Product.Add(new ProductModel { nm_product = "produtoTeste2", category_product = "categoriaProduto2" });
+            _context.Product.Add(new ProductModel
+            {
+                nm_product = "produtoTeste1",
+                category_product = "categoriaProduto1"
+            });
+            _context.Product.Add(new ProductModel
+            {
+                nm_product = "produtoTeste2",
+                category_product = "categoriaProduto2"
+            });
+
             _context.SaveChanges();
 
             // Act
@@ -39,7 +50,137 @@ namespace VOM_HIVE.API.TESTS.Tests
             var json = await response.Content.ReadFromJsonAsync<ResponseModel<List<ProductModel>>>();
 
             Assert.NotNull(json.Dados);
-            Assert.NotEmpty(json.Dados);  // Verifica que a lista de produtos não está vazia
+            Assert.NotEmpty(json.Dados);
+        }
+
+        [Fact]
+        public async Task GetProductByIdProduct_ReturnProduct()
+        {
+            // Arrange
+            int id_product = 1;
+            _context.Product.Add(new ProductModel
+            {
+                nm_product = "produtoTeste1",
+                category_product = "categoriaProduto1"
+            });
+            _context.Product.Add(new ProductModel
+            {
+                nm_product = "produtoTeste2",
+                category_product = "categoriaProduto2"
+            });
+
+            _context.SaveChanges();
+
+            // Act
+            var response = await _client.GetAsync($"/api/Product/BuscarProdutoPorId/{id_product}");
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            var json = await response.Content.ReadFromJsonAsync<ResponseModel<ProductModel>>();
+
+            Assert.NotNull(json.Dados);
+        }
+
+        [Fact]
+        public async Task GetProductByIdProduct_ReturnNull_WhenDoenstExist()
+        {
+            // Arrange
+            int id_product = 6699;
+
+            // Act
+            var response = await _client.GetAsync($"/api/Product/BuscarProdutoPorId/{id_product}");
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            var json = await response.Content.ReadFromJsonAsync<ResponseModel<ProductModel>>();
+
+            Assert.Null(json.Dados);
+        }
+
+        [Fact]
+        public async Task GetProductByIdCompaign_ReturnProduct()
+        {
+            // Arrange
+            var company = new CompanyModel
+            {
+                nm_company = "BotaCola Inc.",
+                cnpj = "12345678910",
+                email = "jaun@company.com.br",
+                dt_register = DateTime.Now
+            };
+
+            var product = new ProductModel
+            {
+                nm_product = "BotaCola",
+                category_product = "Bebida"
+            };
+
+            _context.Company.Add(company);
+            _context.Product.Add(product);
+            _context.SaveChanges();
+
+            var campaign = new CampaignModel
+            {
+                nm_campaign = "Cola ni mim, cola em nóis",
+                target = "adultos",
+                dt_register = DateTime.Now,
+                details = "alimentos",
+                status = "ativo",
+                id_product = product.id_product,
+                id_company = company.id_company
+            };
+
+            _context.Campaign.Add(campaign);
+            _context.SaveChanges();
+
+            // Act
+            var response = await _client.GetAsync($"/api/Product/BuscarProdutoPorIdCampaign/{campaign.id_campaign}");
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            var json = await response.Content.ReadFromJsonAsync<ResponseModel<ProductModel>>();
+
+            Assert.NotNull(json.Dados);
+        }
+
+        [Fact]
+        public async Task GetProductByIdCompaign_ReturnNull_WhenCampaignDoenstExist()
+        {
+            // Arrange
+            int id_campaign = 6699;
+
+            // Act
+            var response = await _client.GetAsync($"/api/Product/BuscarProdutoPorIdCampaign/{id_campaign}");
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            var json = await response.Content.ReadFromJsonAsync<ResponseModel<ProductModel>>();
+
+            Assert.Null(json.Dados);
+        }
+
+        [Fact]
+        public async Task CreateProduct_ReturnsCreatedProduct()
+        {
+            // Arrange
+            var product = new ProductModel
+            {
+                nm_product = "BotaCola",
+                category_product = "Bebida"
+            };
+
+            // Act
+            var response = await _client.PostAsJsonAsync("/api/Product/CreateProduct", product);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+
+            var jsonProduct = await response.Content.ReadFromJsonAsync<ResponseModel<ProductModel>>();
+
+
+            Assert.Equal(product.nm_product, jsonProduct.Dados.nm_product);
+            //Assert.Equal(product.category_product, product.category_product);
         }
     }
 }
