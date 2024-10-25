e3ad7b3601f21a8df4ecef9db253bfd72d1982c5
gabrielrodri33 <gabrielrodri333@gmail.com>
2024-10-25 15:59:50 -0300

    Configurado para retornarem apenas o necessário quando forem utilizados

--- VOM-HIVE.API/Services/Product/IProductInterface.cs
+++ VOM-HIVE.API/Services/Product/IProductInterface.cs
@@ -9,8 +9,8 @@ namespace VOM_HIVE.API.Services.Product
         Task<ResponseModel<List<ProductModel>>> ListarProdutos();
         Task<ResponseModel<ProductModel>> BuscaProdutoPorId(int id_product);
         Task<ResponseModel<ProductModel>> BuscarProdutoPorIdCampaign(int id_campaign);
-        Task<ResponseModel<List<ProductModel>>> CreateProduct(ProductCreateDto productCreateDto);
-        Task<ResponseModel<List<ProductModel>>> EditProduct(ProductEditDto productEditDto);
-        Task<ResponseModel<List<ProductModel>>> DeleteProduct(int id_product);
+        Task<ResponseModel<ProductModel>> CreateProduct(ProductCreateDto productCreateDto);
+        Task<ResponseModel<ProductModel>> EditProduct(ProductEditDto productEditDto);
+        Task<ResponseModel<ProductModel>> DeleteProduct(int id_product);
     }
 }
