002ca765580c8c7bc8735e6e801e77339ae5c0ee
gabrielrodri33 <gabrielrodri333@gmail.com>
2024-10-30 14:26:42 -0300

    Configuração de Authorization no Swagger

---
+++ VOM-HIVE.API/Dependecy/DependecyInjectionSwagger.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace VOM_HIVE.API.Dependecy
+{
+    public static class DependecyInjectionSwagger
+    {
+        public static IServiceCollection AddInfrastructureSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "Please enter the token with Bearer in front of it.",
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme()
+                        {
+                            Reference = new OpenApiReference()
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] {}
+                    }
+                });
+            });
+            return services;
+        }
+    }
+}
