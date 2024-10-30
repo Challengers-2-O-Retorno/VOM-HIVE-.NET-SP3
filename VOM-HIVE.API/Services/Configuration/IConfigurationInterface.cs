a643561f4ddbf7954d10a06a4e0a7321a906944f
gabrielrodri33 <gabrielrodri333@gmail.com>
2024-10-30 14:27:52 -0300

    Service e Interface de Configuration, para capturar credenciais de appsettings.json

---
+++ VOM-HIVE.API/Services/Configuration/IConfigurationInterface.cs
@@ -0,0 +1,10 @@
+﻿namespace VOM_HIVE.API.Services.Configuration
+{
+    public interface IConfigurationInterface
+    {
+        string ClientId { get; }
+        string ClientSecret { get; }
+        string Audience { get; }
+        string Authority { get; }
+    }
+}
