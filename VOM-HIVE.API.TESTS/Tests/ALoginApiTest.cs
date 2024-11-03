a0bf20ac35c021ce9547175a7a00c0de0716bedf
gabrielrodri33 <gabrielrodri333@gmail.com>
2024-11-02 23:30:09 -0300

    Configurado Teste para Login

---
+++ VOM-HIVE.API.TESTS/Tests/ALoginApiTest.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Sp2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using VOM_HIVE.API.Auth;
+using VOM_HIVE.API.Controllers;
+using VOM_HIVE.API.Data;
+using VOM_HIVE.API.Models;
+using VOM_HIVE.API.Services.Configuration;
+using VOM_HIVE.API.TESTS.Data;
+using Xunit.Priority;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace VOM_HIVE.API.TESTS.Tests
+{
+    [Collection("ApiTests")]
+    public class ALoginApiTest : IClassFixture<CustomWebApplicationFactory<Program>>
+    {
+        private readonly HttpClient _client;
+        private readonly AppDbContext _context;
+
+        public ALoginApiTest(CustomWebApplicationFactory<Program> factory)
+        {
+            _client = factory.CreateClient();
+
+            var scope = factory.Services.CreateScope();
+            _context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        }
+
+        private string _token;
+
+        [Fact]
+        public async Task ALogin_ReturnsToken_WhenCredentialsAreValid()
+        {
+            //Arrange
+            var company = new CompanyModel
+            {
+                nm_company = "BotaCola Inc.",
+                cnpj = "12345678910",
+                email = "jaun@company.com.br",
+                dt_register = DateTime.Now
+            };
+
+            _context.Company.Add(company);
+            _context.SaveChanges();
+
+            var profileUser = new ProfileuserModel
+            {
+                nm_user = "teste",
+                pass_user = "password",
+                dt_register = DateTime.Now,
+                permission_user = "TODAS",
+                status = "MORTO",
+                id_company = company.id_company
+            };
+
+            _context.Profile_user.Add(profileUser);
+            _context.SaveChanges();
+
+            var login = new LoginModel
+            {
+                nm_user = profileUser.nm_user,
+                pass_user = profileUser.pass_user,
+            };
+
+            //Act
+            var response = await _client.PostAsJsonAsync($"/api/Login/Login", login);
+
+            //Assert
+            response.EnsureSuccessStatusCode();
+            var token = await response.Content.ReadFromJsonAsync<TokenModel>();
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.NotNull(token);
+
+            Assert.NotNull(token.token);
+
+            TokenStorage.Instance.Token = token.token;
+        }
+    }
+
+    public class TokenModel
+    {
+        public string token { get; set; }
+    }
+
+    public class TokenStorage
+    {
+        public static TokenStorage _instance;
+        public static readonly object _lock = new object();
+
+        public static TokenStorage Instance
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    return _instance ??= new TokenStorage();
+                }
+            }
+        }
+        public string Token { get; set; }
+    }
+}
