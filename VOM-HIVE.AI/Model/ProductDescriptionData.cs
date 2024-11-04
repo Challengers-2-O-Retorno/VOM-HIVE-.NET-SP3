de5d314ac883f23f5db2a30c35684f0cb5bdeb0e
gabrielrodri33 <gabrielrodri333@gmail.com>
2024-11-04 15:32:29 -0300

    Criado a classe com atributo de Description da IA

---
+++ VOM-HIVE.AI/Model/ProductDescriptionData.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.ML.Data;
+
+namespace VOM_HIVE.AI.Model
+{
+    public class ProductDescriptionData
+    {
+        public string Description { get; set; }
+    }
+
+    public class ProductDescriptionPrediction
+    {
+        [ColumnName("PredictedLabel")]
+        public string PredictedDescription { get; set; }
+    }
+}
