f93d3958d91e760762d61b96b201befe1b77211f
gabrielrodri33 <gabrielrodri333@gmail.com>
2024-11-04 15:33:56 -0300

    Criado Service da IA

---
+++ VOM-HIVE.API/Services/ProductDescription/ProductDescriptionService.cs
@@ -0,0 +1,33 @@
+﻿using VOM_HIVE.AI.Engine;
+using VOM_HIVE.AI.Model;
+
+namespace VOM_HIVE.API.Services.ProductDescription
+{
+    public class ProductDescriptionService
+    {
+        private readonly MarkovChainGenerator _markovChainGenerator;
+        private readonly List<string> _descriptions;
+
+        public ProductDescriptionService(IEnumerable<ProductDescriptionData> trainingData)
+        {
+            _markovChainGenerator = new MarkovChainGenerator(trainingData);
+            _descriptions = trainingData.Select(d => d.Description).ToList();
+        }
+
+        //public string GenerateDescription(string category, string features)
+        //{
+        //    string startingPhrase = $"O produto na categoria {category} possui as seguintes características: {features}.";
+        //    return _markovChainGenerator.GenerateDescription(startingPhrase);
+        //}
+
+        public string GenerateDescription(string category, string features)
+        {
+            // Cria uma frase inicial com base na categoria e nas características
+            var startingPhrase = $"O produto é um {category} que oferece {features}.";
+            string generatedDescription = _markovChainGenerator.GenerateDescription(startingPhrase);
+
+            // Retorna a descrição gerada, mas verifica se é vazia ou não
+            return string.IsNullOrWhiteSpace(generatedDescription) ? "Descrição não gerada. Tente novamente." : generatedDescription;
+        }
+    }
+}
