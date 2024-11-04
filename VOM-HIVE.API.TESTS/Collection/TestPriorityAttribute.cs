ce45e0c47fbf7d0385f2a17fbc93adf628d7d4d9
gabrielrodri33 <gabrielrodri333@gmail.com>
2024-11-04 16:50:44 -0300

    Limpeza de importações desnecessárias

--- VOM-HIVE.API.TESTS/Collection/TestPriorityAttribute.cs
+++ VOM-HIVE.API.TESTS/Collection/TestPriorityAttribute.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace VOM_HIVE.API.TESTS.Collection
+﻿namespace VOM_HIVE.API.TESTS.Collection
 {
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
     public class TestPriorityAttribute : Attribute
     {
         public int Priority { get; }
-        public TestPriorityAttribute (int priority) => Priority = priority;
+        public TestPriorityAttribute(int priority) => Priority = priority;
     }
 }
